8550f482cd801bef153296a171fac22ec8495157
Siva70136 <94431863+Siva70136@users.noreply.github.com>
2024-03-18 18:30:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors()); 
+app.use(cors({
+    origin: 'http://localhost:3001'
+}));
 app.use(bodyParser.json());
 
 
